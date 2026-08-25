babec1996a55f81544befab2a7e0808c51f1da61
NOVABOTV1F <aminaputra6@gmail.com>
2026-08-25 18:29:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const TOKENS = {
-  github: "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",
+  github: "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",
   netlify: "nfp_YK8x82uUZo2x1o9G79dGjc3hrojSTmU92f43",
   vercel: ""
 };
